5b98e9c1f4a325556ed73b4b9630fcb42872dd2c
sayanbofc07 <sayanb.ofc07@gmail.com>
2025-05-18 14:35:32 +0530

    local

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+import uuid
 
 def get_windows_host_ip():
     try:
@@ -15,8 +17,34 @@ db_pass = "Ams_12345"
 db_name = "test_db"
 
 engine = create_engine(f"mysql+pymysql://{db_user}:{db_pass}@{windows_ip}:3306/{db_name}")
+# id_ = "06e06d09-9c41-44b6-bb07-611646817c9e"
+id_ = "1' OR '1'='1" ## sql injection
+name = "zuu"
 
+## for core
 with engine.connect() as connection:
-    result = connection.execute(text("SELECT * FROM users LIMIT 1"))
-    row = result.fetchone()
-    print(row)
+    # result = result = connection.execute(text(f"SELECT * FROM users where id = '{id_}'"))
+    result = connection.execute(text("SELECT * FROM users WHERE id = :id and name = :name"), {"id": id_, "name":name})
+    results = result.mappings().all()
+    if not results:
+        connection.execute(
+            text("INSERT INTO users (id, name, email) VALUES (:id, :name, :email)"),
+            [{"id": str(uuid.uuid4()), "name": 'abc3', "email": "test@abc.com"}, {"id": str(uuid.uuid4()), "name": 'abc4', "email": "test2@abc.com"}],
+        )
+        connection.commit()
+        print("data inserted...")
+
+## for orm
+SessionLocal = sessionmaker(bind=engine)
+session = SessionLocal()
+
+result = session.execute(text("SELECT * FROM users WHERE id = :id and name = :name"), {"id": id_, "name":name})
+results = result.mappings().all()
+
+session.execute(
+            text("INSERT INTO users (id, name, email) VALUES (:id, :name, :email)"),
+            [{"id": str(uuid.uuid4()), "name": 'abc7', "email": "test@abc.com"}, {"id": str(uuid.uuid4()), "name": 'abc8', "email": "test2@abc.com"}],
+        )
+session.commit()
+session.close()
+print("data inserted...")
